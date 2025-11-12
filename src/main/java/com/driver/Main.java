cf338e0e32ca6610e3d56e60c27c9ac8399b13de
asif <asifkhan68138@gmail.com>
2025-11-12 09:36:02 +0530

    assingnment completed

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,28 @@
 package com.driver;
-
+class Product{
+    public int product(int x,int y){
+        return x*y;
+    };
+    public int product(int x,int y,int z){
+      return x*y*z;
+    };
+    public double product(double x,double y){
+        return x*y;
+    };
+}
 public class Main {
 
+
+    public static void main(String[] args) {
+        Product p=new Product();
+
+        int x=2;
+        int y=3;
+        int z=4;
+
+        System.out.println("first " + p.product(x,y));
+        System.out.println("Second " +  p.product(x,y,z));
+        System.out.println("third " + p.product(2.5,4.6));
+
+    }
 }
