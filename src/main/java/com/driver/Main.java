dc7e5260802293eacac3896d275fab9a981a2d56
asif <asifkhan68138@gmail.com>
2025-11-12 10:09:00 +0530

    poly assing completed attempt 5

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,21 +1,22 @@
 package com.driver;
-
 public class Main {
-    static class Product{
-        public int product(int x,int y){
+    public static class Product{
+        public int product(int x, int y){
             return x*y;
         }
-        public int product(int x,int y,int z){
+
+        public int product(int x, int y, int z){
             return x*y*z;
         }
-        public double product(double x,double y){
+
+        public double product(double x, double y){
             return x*y;
         }
     }
-    public static void main(String[] args) {
-        Product p=new Product();
-        System.out.println(p.product(2,3));
-        System.out.println(p.product(2,3,4));
-        System.out.println(p.product(2.5,4.6));
-    }
+    public static void main(String[] args){
+        Product p = new Product();
+        System.out.println(p.product(2, 3));
+        System.out.println(p.product(3,4,5));
+        System.out.println(p.product(4.5,6.6));
+}
 }
